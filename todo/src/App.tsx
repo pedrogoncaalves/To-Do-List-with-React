acac4c6e4d8ef912748b0066e4b2c6f7fe3e922e
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-08-24 14:02:40 -0300

    second commit

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Importando o useState pra usar variaveis dinamicas
+import { useState } from "react";
+// Importando os styled components do arquivo styles
+import * as C from "./App.Styles";
+// Importanto types
+import { Item } from "./types/item"
+// Importando componentes
+import { ListItem } from "./components/ListItem"
 
-function App() {
+
+// Criando lista de tarefas através do useState do React Hooks
+const App =  () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Comprar o Pão na Padaria', done: false},
+    {id: 2, name: 'Comprar o Bolo na Padaria', done: false}
+  ]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+      <C.Area> 
+        <C.Header>Lista de Tarefas</C.Header>
+
+
+
+
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item}/>
+        ))}
+
+      </C.Area>
+    </C.Container>
+
+
+  )
+
 }
+
+
 
 export default App;
---
+++ todo/src/components/ListItem/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+background-color: #9873AC;
+padding: 10px;
+border-radius: 10px;
+margin-bottom: 10px;
+align-items: center;    
+
+
+input {
+    width: 20px;
+    height: 20px;
+    margin-right: 3px;
+}
+
+label {
+    color: #FFFFFF;
+}
+
+`
